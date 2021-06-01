f4b675550385263225b8f5bd8cf2a9815b8a8862
Morrys-py <nelmauriciolop@gmail.com>
2021-06-01 08:38:42 -0300

    chicos esto es lo que hice yo

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -15,10 +15,9 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
+      {path: '/index/', url: 'index.html', },
+      {path: '/anotador/', url: 'anotador.html', },
+
     ]
     // ... other parameters
   });
@@ -33,7 +32,7 @@ $$(document).on('deviceready', function() {
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
-    console.log(e);
+    //console.log(e);
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
@@ -42,3 +41,37 @@ $$(document).on('page:init', '.page[data-name="about"]', function (e) {
     console.log(e);
     alert('Hello');
 })
+
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log("Inicia index");
+    $$("#IrAAnotador").on('click', fnIrAAnotador);
+})
+
+$$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log("Inicia anotador");
+    $$("#volverInicio").on('click', fnIrAInicio);
+    $$('#j1Nombre').html(jugador1);
+    $$('#j2Nombre').html(jugador2);
+})
+
+var jugador1 ="";
+var jugador2 ="";
+
+function fnIrAAnotador() {
+    jugador1 = $$('#IndexJugador1').val();
+    jugador2 = $$('#IndexJugador2').val();
+    if (jugador1!="" && jugador2!="") {
+        mainView.router.navigate('/anotador/');
+        $$('#volverInicio').removeClass('oculto').addClass('visible');
+    } else {
+        alert("Completa ambos campos!")
+    }
+    
+}
+
+function fnIrAInicio(){
+    mainView.router.navigate('/index/');
+    $$('#volverInicio').removeClass('visible').addClass('oculto');
+}
